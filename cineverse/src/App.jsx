ca26cc26b940890143cd62844a22129166206813
AyushSinha2603 <ayushcodes26@gmail.com>
2025-06-07 01:29:48 +0530

    initilased

---
+++ cineverse/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const Card = ({ title }) => {
+  const [count, setcount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect( () => {
+    console.log(`${title} has been liked : ${hasLiked}`);
+  }, [hasLiked]);
+
+  return (
+    <div className="card" onClick={() =>setcount(count + 1)}>
+      <h2>{title} <br/> {count ? count : null}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? 'Liked' : 'Like'}
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  )
+}
+
+export default App
